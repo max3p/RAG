77dc7e2b5ae618e2ea742c9b82384dbf068eeb23
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2025-02-23 13:57:25 -0700

    changed embedding function to local

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -5,8 +5,6 @@ from langchain_community.embeddings.bedrock import BedrockEmbeddings
 # Text embedding function from AWS Bedrock
 # Text use Ollama (locally hosted) but its not as good
 def get_embedding_function():
-    embeddings = BedrockEmbeddings(
-        credentials_profile_name="default", region_name="us-east-1"
-    )
-    # embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    #embeddings = BedrockEmbeddings(credentials_profile_name="default", region_name="us-east-1")
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
     return embeddings
